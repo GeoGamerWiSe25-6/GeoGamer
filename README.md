ed71a180645d9b58c51f760bb5c94e88b9645101
mzahed <mzahed@stud.hs-bremen.de>
2025-12-10 14:48:01 +0100

    Zufällige Orte

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # GeoGamer
 Projekt für das Modul Geodatenverarbeitung WiSe 25/26
+
+### Overpass Query für Orte in Deutschland (https://overpass-turbo.eu/)
+[out:json];
+area["ISO3166-1"="DE"]->.de;
+(
+node["place"~"city|town"](area.de);
+way["place"~"city|town"](area.de);
+relation["place"~"city|town"](area.de);
+);
+out center;
+
+Das Ergebnis ist gespeichert in data/export_german_cities_towns.geojson
+
--- app.js
+++ app.js
@@ -24,62 +24,21 @@ function getRandomLocationGermany(){
     return [lat,lon];
 }
 
-async function hasBuildingNearby(lat, lon, radius=200) {
-    const query = `
-    [out:json][timeout:10];
-    (
-    way["building"](around:${radius}, ${lat}, ${lon});
-    relation["building"](around:${radius}, ${lat}, ${lon});
-    );
-    out center;
-    `;
+async function loadLocations(){
+    await view.when();
+    const response = await fetch("data/export_german_cities_towns.geojson");
+    const data = await response.json();
+    const features = data.features;
+    const randomFeature = features[Math.floor(Math.random()*features.length)];
 
-    const url = "https://overpass-api.de/api/interpreter";
-
-    try {
-        const response = await fetch(url, {
-            method: "POST",
-            body: query,
-        });
-
-        const contentType = response.headers.get("content-type");
-        if (!contentType || !contentType.includes("json")) {
-            console.error("Overpass hat kein JSON zurückgegeben", await response.text());
-            return false;
-        }
-
-        const data = await response.json();
-        // ob was gefunden wurde
-        return data.elements && data.elements.length > 0;
-    } catch (e) {
-        console.error("Overpass error: ", e)
-        return false;
-    }
-}
-
-async function getPlausibleLocationGermany(maxAttempts=20){
-    for (let attempt=1; attempt<= maxAttempts; attempt++){
-        const [lat, lon] = getRandomLocationGermany();
-        const ok = await hasBuildingNearby(lat, lon);
-
-        if(ok) {
-            console.log(`Passende Koordinate ${lat}, ${lon} nach ${attempt}.ten Versuch. `);
-            return {lat, lon};
-        }
-    }
-    console.warn("Keine geeigneten Koordinaten gefunden");
-    return { lat:8.79, lon:53.075}; // Bremen als Fallback
-}
-
-async function jumpToRandomLocation(){
-    const loc = await getPlausibleLocationGermany();
+    const lat = randomFeature.geometry.coordinates[1];
+    const lon = randomFeature.geometry.coordinates[0];
+    console.log("Zufälliger Ort: ", randomFeature.properties.name);
 
     view.goTo({
-        center: [loc.lon, loc.lat],
+        center : [lon, lat],
         zoom: 16
     });
 }
 
-view.when(() => {
-   jumpToRandomLocation();
-});
+loadLocations();
