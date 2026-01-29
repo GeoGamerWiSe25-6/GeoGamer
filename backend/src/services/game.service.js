95e0cf51762cd8a4873a48f1b0eedb1bad888e9f
mj951310 <mzahed@stud.hs-bremen.de>
2026-01-29 22:37:35 +0100

    Frontend mit DB verbunden

--- backend/src/services/game.service.js
+++ backend/src/services/game.service.js
@@ -1,5 +1,5 @@
 import {pool} from "../config/db.js"
-import {scoreMap} from "../utils/geo.utils.js"
+import {scoreMap, distanceMap} from "../utils/geo.utils.js"
 
 export async function startRound(){
     const client  = await pool.connect();
@@ -17,23 +17,31 @@ export async function startRound(){
 
         // zufälliger Punkt in Gemeinde
         const p = await client.query(`
-       SELECT ST_AsGeoJSON( ST_PointOnSurface(geom) ) 
+       SELECT ST_PointOnSurface(geom) 
        AS point
        FROM communities_clean
        WHERE id= $1
        `, [communityId]);
 
-        const point = JSON.parse(p.rows[0].point);
-        const [lon, lat] = point.coordinates;
+        const point25832 = p.rows[0].point;
 
         const round = await client.query(`
        INSERT INTO rounds (target_community_id, target_point)
-       VALUES ($1, ST_SetSRID(ST_Point($2, $3), 25832))
+       VALUES ($1, $2)
        RETURNING id
-       `, [communityId, lon, lat]);
+       `, [communityId, point25832]);
 
         const roundId = round.rows[0].id;
 
+        // Für Leaflet FE auf ESPG 4326 umwandeln
+        let point4326 = await client.query(`
+            SELECT ST_AsGeoJSON(
+                ST_TRANSFORM($1::geometry, 4326)
+            ) AS point
+        `,[point25832]);
+
+        point4326 = JSON.parse(point4326.rows[0].point)
+        const [lon, lat] = point4326.coordinates;
         return {
                         roundId: roundId,
                         view: {
@@ -51,8 +59,15 @@ export async function guessCommunity(roundId, guessLat, guessLon){
     const guessedCommunityResult = await pool.query(`
         SELECT id
         FROM communities_clean g
-        WHERE ST_Contains(g.geom, ST_SetSRID(ST_Point($1, $2), 25832))
+        WHERE ST_Contains(g.geom,
+                          ST_Transform( ST_SetSRID(ST_Point($1, $2), 4326),
+                                        25832)
+              )
     `, [guessLon, guessLat]);
+
+    if(guessedCommunityResult.rows.length===0){
+        throw new Error("Guess outside all communities");
+    }
 
     const guessedCommunityId = guessedCommunityResult.rows[0].id;
 
@@ -61,10 +76,12 @@ export async function guessCommunity(roundId, guessLat, guessLon){
     `, [roundId, guessedCommunityId]);
 
     const distanceClass = result.rows[0].distance_class;
+    const distanceClassName = distanceMap[distanceClass];
 
     return {
                 distanceClass,
                 score: scoreMap[distanceClass],
+                distanceClassName,
                 community: result.rows[0].name
     };
 }
--- backend/src/utils/geo.utils.js
+++ backend/src/utils/geo.utils.js
@@ -5,3 +5,11 @@ export const scoreMap ={
     3: 100,
     4: 0
 };
+
+export const distanceMap = {
+    0: "richtige Gemeinde",
+    1: "angrenzende Gemeinde",
+    2: "bis zu 20km",
+    3: "bis zu 50km",
+    4: "über 50km"
+}
